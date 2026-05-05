408b2b58f80b1138b3d79f1fac05d3f918ef5842
Xavanium <xavanium@gmail.com>
2026-05-05 22:17:16 +0000

    h

---
+++ complexComparison.cpp
@@ -0,0 +1,20 @@
+//calculator to compare the absolute values of complex numbers
+
+#include <iostream>
+#include <complex>
+#include <cmath>
+#include <iomanip>
+int main() {
+    double r1, i1, r2, i2;
+    std::cout << "Input the first real number: ";
+    std::cin >> r1;
+    std::cout << "Input the imaginary coordinate: ";
+    std::cin >> i1;
+    std::cout << "Input the second real number: ";
+    std::cin >> r2;
+    std::cout << "Input the second imaginary coordinate: ";
+    std::cin >> i2;
+    std::complex<double> z(r1, i1);
+    std::complex<double> w(r2, i2);
+    return 0;
+}
