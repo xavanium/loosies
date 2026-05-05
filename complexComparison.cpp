0ab737860845216cf6234ebe7b08f17d3495cd84
Xavanium <xavanium@gmail.com>
2026-05-05 22:19:45 +0000

    Add magnitude calculation for complex numbers

--- complexComparison.cpp
+++ complexComparison.cpp
@@ -14,7 +14,9 @@ int main() {
     std::cin >> r2;
     std::cout << "Input the second imaginary coordinate: ";
     std::cin >> i2;
-    std::complex<double> z(r1, i1);
+    std::complex<double> z(r1, i1); //defines complex number as r1 + i1
     std::complex<double> w(r2, i2);
+    double magnitude = std::abs(z); //automatically calculates sqrt(a^2 + b^2) aka distance
+    double magni2de = std::abs(w);
     return 0;
 }
