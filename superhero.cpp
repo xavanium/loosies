ac8efa63b8d2360e27fa3c9a730ed5d52516d053
Xavanium <xavanium@gmail.com>
2026-06-12 18:59:01 +0000

    Refactor user_hero class to initialize attributes in constructor and streamline object creation in main

--- superhero.cpp
+++ superhero.cpp
@@ -13,20 +13,21 @@ class user_hero {
             std::cout << "POWER: " << superpower << std::endl;
             std::cout << "AGE: " << age << " YEARS OLD" << std::endl;
         }
-
+        user_hero(string name, string power, int age){
+            heroName = name;
+            superpower = power;
+            this->age = age;
+        }
 };
 int main() {
     string name, power;
     int age;
-    user_hero hero;
     std::cout<<"What is your superhero name? ";
     std::cin >> name;
     std::cout << "What superpower do you have? ";
     std::cin >> power;
     std::cout << "How old are you? ";
     std::cin >> age;
-    hero.heroName = name;
-    hero.superpower = power;
-    hero.age = age;
+    user_hero hero = user_hero(name, power, age);
     hero.hero_chart();
 }
