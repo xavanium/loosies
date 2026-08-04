a4e70469f89f8b7ca1e67d72c5785da9538bfb53
Xavanium <xavanium@gmail.com>
2026-08-04 18:57:26 +0000

    still not workin wtf!

--- collatzConjecture.cpp
+++ collatzConjecture.cpp
@@ -2,16 +2,16 @@
 //collatz conjecture simulator
 
 int main(){
-    int n;
+    int i;
     std::cout << "Input any integer: ";
-    std::cin >> n;
-    while (n>=1){
-        if (n%2){
-            n = n*3 + 1;
-            std::cout << n << std::endl;
+    std::cin >> i;
+    while (i!=1){
+        if (i%2){
+            i = i*3 + 1;
+            std::cout << i << std::endl;
         } else {
-            n = n/2;
-            std::cout << n << std::endl;
+            i = i/2;
+            std::cout << i << std::endl;
         }
     }
     return 0;
